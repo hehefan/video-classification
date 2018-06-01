9080adf6a87e0854f5b676131cff83e5842574d6
Hehe Fan <crane.h.fan@gmail.com>
2018-06-01 14:38:34 +1000

    Update ucf-101.sh

--- data/ucf-101.sh
+++ data/ucf-101.sh
@@ -1,2 +1,8 @@
 wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
-unrar e UCF101.rar videos/
+unrar e UCF101.rar ucf101-videos/
+rm UCF101.rar
+
+for video in `ls ucf101-videos`; do
+  mkdir -p ucf101-frames/$video
+  ffmpeg -i ucf101-videos/$video -vf fps=1 ucf101-frames/$video/%04d.jpg
+done
