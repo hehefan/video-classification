18f4fda2f24e112713bffdb4d00671f84642e8a7
Hehe Fan <crane.h.fan@gmail.com>
2018-06-01 15:01:10 +1000

    Update ucf-101.sh

--- data/ucf-101.sh
+++ data/ucf-101.sh
@@ -2,6 +2,10 @@ wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
 unrar e UCF101.rar ucf101-videos/
 rm UCF101.rar
 
+wget http://crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
+unzip UCF101TrainTestSplits-RecognitionTask.zip
+rm unzip UCF101TrainTestSplits-RecognitionTask.zip
+
 for video in `ls ucf101-videos`; do
   mkdir -p ucf101-frames/$video
   ffmpeg -i ucf101-videos/$video -vf fps=1 ucf101-frames/$video/%04d.jpg
