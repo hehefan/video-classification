43722ee688950e024971cfbedad468fbb031d0d8
Hehe Fan <crane.h.fan@gmail.com>
2018-06-19 22:29:11 +1000

    Update config.py

--- pooling/config.py
+++ pooling/config.py
@@ -7,8 +7,8 @@ tf.app.flags.DEFINE_float("min_learning_rate", 1e-4, "Minimal learning rate.")
 tf.app.flags.DEFINE_integer("batch_size", 32, "Batch size to use during training.")
 tf.app.flags.DEFINE_float("keep_prob", 0.5, "Dropout between the last state and the full connection.")
 
-
 tf.app.flags.DEFINE_integer("feature_size", 2048, "Size of frame feature.")
+tf.app.flags.DEFINE_integer("video_length", 5, "Length of video.")
 
 tf.app.flags.DEFINE_integer("num_classes", 101, "Number of classes.")
 
